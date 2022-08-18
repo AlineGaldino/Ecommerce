777f1802b41ca23ecbc1db44bc06a8d861449c5d
Aline Galdino <78483899+AlineGaldino@users.noreply.github.com>
2022-08-18 17:20:58 -0300

    feat: add preview products page

---
+++ src/pages/Preview/index.js
@@ -0,0 +1,11 @@
+import Navbar from '../../component/Navbar'
+import {Products} from '../../component/Products'
+
+export default function Preview() {
+  return (
+    <>
+    <Navbar/>
+    <Products/>
+    </>
+  )
+}
