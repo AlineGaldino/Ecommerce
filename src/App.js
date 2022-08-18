23cf991050d5da7e493432f89ec8d612411a1713
Aline Galdino <78483899+AlineGaldino@users.noreply.github.com>
2022-08-18 19:22:59 -0300

    feat: add cart route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 //import Navbar from './component/Navbar';
 import Home from './component/Home';
+import Cart from './pages/Cart';
 import Preview from './pages/Preview';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/products' element={<Preview/>} />
+        <Route path='/cart' element={<Cart/>} />
       </Routes>
     </Router>
   );
