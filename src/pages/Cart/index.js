6d7e3736098f0730f9feed471414b6d6ec2346c9
Aline Galdino <78483899+AlineGaldino@users.noreply.github.com>
2022-08-18 19:28:56 -0300

    feat: add cart

---
+++ src/pages/Cart/index.js
@@ -0,0 +1,11 @@
+import Navbar from '../../component/Navbar'
+import EmptyCart from '../../component/EmptyCart'
+
+export default function Cart() {
+    return (
+      <>
+        <Navbar/>
+        <EmptyCart/>
+      </>
+    )
+}
