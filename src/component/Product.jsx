070054bb33f4f79109de0737e5f3ae61954c8e85
Aline Galdino <78483899+AlineGaldino@users.noreply.github.com>
2022-08-19 16:25:26 -0300

    style: products for preview

--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -1,12 +1,28 @@
-import { Text } from "@chakra-ui/react"
-import { Box } from "@chakra-ui/react"
+import { Flex, Box, Img, Checkbox, Text, Button, Divider } from "@chakra-ui/react"
+
+import shirt from '../images/imaget-shirt.svg'
 
 export function Product({description, longDescription, price}) {
     return (
+    <Flex margin='0px 400px 0px 400px' marginBottom='25px'>
+        <Checkbox defaultChecked paddingRight='25px'/>
+        <Img src={shirt} alt='t-shirt' paddingRight='25px'/>
         <Box>
-            <Text>{description}</Text>
+            <Text fontSize='34px'>{description}</Text>
             <Text>{longDescription}</Text>
-            <Text>{price}</Text>
+            <Text as='em' textTransform='uppercase'>r$ {price}/1 un</Text>
+            <Divider />
         </Box>
+        <Flex alignItems='center'>
+            <Button
+            bg='#170063'
+            w='150px'
+            h='53px'
+            color='white'
+            boxShadow='3px 3px 5px gray'>
+            + Carrinho
+            </Button>
+        </Flex>
+    </Flex>
     )
 }
--- src/pages/Preview/index.js
+++ src/pages/Preview/index.js
@@ -1,10 +1,32 @@
 import Navbar from '../../component/Navbar'
 import {Products} from '../../component/Products'
 
+import { Flex, Button } from '@chakra-ui/react'
+
 export default function Preview() {
   return (
     <>
     <Navbar/>
+      <Flex
+        padding='20px'
+        flexDirection='row'
+        justifyContent='center'>
+        <Button
+          bg='white'
+          color='#170063'
+          w='235px'
+          h='53px'>
+          Selecionar todos
+        </Button>
+        <Button
+          bg='#170063'
+          color='white'
+          w='235px'
+          h='53px'
+          boxShadow='3px 3px 5px gray'>
+          Adicionar selecionados
+        </Button>
+      </Flex>
     <Products/>
     </>
   )
