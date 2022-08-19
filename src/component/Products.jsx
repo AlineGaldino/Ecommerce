817573a46e0ad2c6c67c91f9174e31e082783122
Aline Galdino <78483899+AlineGaldino@users.noreply.github.com>
2022-08-18 21:50:00 -0300

    fix: products

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -1,31 +1,37 @@
-import  {Product} from './Product'
-import products from '../products.json'
+import { useState, useEffect } from 'react';
+import { Product } from './Product'
 import { Box } from '@chakra-ui/react'
 
 export function Products() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    ((async () => {
+      const res = await fetch('http://localhost:3000/products', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+
+      if (res.status === 200) {
+        const data = await res.json();
+        setProducts(data);
+      } else {
+        console.error(res.status);
+      }
+
+    }))();
+  }, [])
+
   return (
     <Box>
-      {products.map((product) => {
-        return (
-          <Product 
-          description={product.description} 
-          longDescription={product.longDescription} 
+      {products.map((product, index) => (
+        <Product
+          key={index}
+          description={product.description}
+          longDescription={product.longDescription}
           price={product.price} />
-        )
-      })}
+      ))}
     </Box>
   )
 }
-
-/*
-export function Products() {
-  return (
-    <Box>
-      {products.map((product) => {
-        return <Product description={product.description} longDescription={product.longDescription} price={product.price}/>;
-      })}
-    </Box>
-  )
-}
-
-*/
